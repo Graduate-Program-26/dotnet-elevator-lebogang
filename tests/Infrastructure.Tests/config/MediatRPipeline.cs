8eb22feeffadd4d4948b35c8aab52350017bb6ac
LebogangMasenya <LMasenya@dvtsoftware.com>
2026-06-17 18:58:48 +0200

    feat: added serilog configuration

--- tests/Infrastructure.Tests/config/MediatRPipeline.cs
+++ tests/Infrastructure.Tests/config/MediatRPipeline.cs
@@ -14,6 +14,7 @@ public class MediatRPipeline
     {
         var services = new ServiceCollection();
 
+        services.AddLogging();
         services.AddApplicationServices();
 
         var mockElevatorRepo = new Mock<IElevatorRepo>();
